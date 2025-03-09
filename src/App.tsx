f834d671bf6473e8a60b2f9693106c93951825e4
k-lila <krishnalilapalandi@gmail.com>
2025-03-08 21:00:53 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyle } from './styles'
+import BlogContainer from './containers'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <BlogContainer>
+        <h1>blog</h1>
+      </BlogContainer>
+    </>
+  )
+}
+
+export default App
---
+++ src/containers/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: black;
+  height: 100svh;
+  padding: 1vmin;
+  .view {
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    border-radius: 1vmin;
+  }
+`
